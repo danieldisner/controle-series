d4dc6f9c0ea8f5b38805501d5ad4e4aba5345942
Daniel Disner <danidisner@gmail.com>
2024-02-01 17:27:10 -0300

    Listagem de Episodios

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\SeasonsController;
+use App\Http\Controllers\EpisodesController;
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,3 +25,8 @@ Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
 Route::get('/series/{series}/seasons',[SeasonsController::class, 'index'])->name('seasons.index');
+Route::get('/seasons/{season}/episodes',[EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes',function (Request $request){
+    dd($request->all());
+});
+
