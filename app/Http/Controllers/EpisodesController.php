9aa9678c0a38e82df36f490387e1b8a6bc292bb4
Daniel Disner <danidisner@gmail.com>
2024-02-02 14:58:13 -0300

    "Marcando Episódios"

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 use App\Models\Season;
+use App\Models\Episode;
+use Illuminate\Http\Request;
+
 class EpisodesController
 {
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', [
+            'episodes' => $season->episodes,
+            'mensagemSucesso' => session('mensagem.sucesso')]);
+    }
+
+    public function update(Request $request, Season $season)
+    {
+        $watchedEpisodes = $request->episodes;
+
+        // Melhorar performance nessa função:
+        // Está disparando várias selects ao mesmo tempo
+        // Porém está funcional
+        $season->episodes->each(function (Episode $episode) use ($watchedEpisodes){
+            $episode->watched = in_array($episode->id, $watchedEpisodes);
+        });
+
+        $season->push();
+
+        return to_route('episodes.index', $season->id)
+        ->with('mensagem.sucesso', "Episódios Marcados como Assistidos");
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,8 +13,23 @@
 <body>
     <div class="container">
         <h1>{{ $title }}</h1>
-
+        @isset($mensagemSucesso)
+        <div class="alert alert-success">
+            {{ $mensagemSucesso }}
+        </div>
+        @endisset
         {{ $slot }}
+
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ( $errors->all() as $error )
+                        <li> {{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+
+        @endif
     </div>
 </body>
 </html>
